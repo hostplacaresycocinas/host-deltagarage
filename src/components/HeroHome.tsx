01d278672cf1489748bc69dad6dfb93519d2d0aa
tobiajras <tobyajras@gmail.com>
2025-08-10 18:24:46 -0300

    ver catalogo font medium

--- src/components/HeroHome.tsx
+++ src/components/HeroHome.tsx
@@ -64,7 +64,7 @@ const HeroHome = () => {
                   company.dark
                     ? 'text-color-title-light bg-color-primary hover:bg-color-primary-dark border-2 border-color-primary hover:border-color-primary-dark'
                     : 'text-color-title-light bg-color-primary hover:bg-color-primary-dark border-2 border-color-primary hover:border-color-primary-dark'
-                } lg:text-lg transition-all duration-300 py-3 md:py-4 px-6 md:px-10 rounded-full font-semibold`}
+                } lg:text-lg transition-all duration-300 py-3 md:py-4 px-6 md:px-10 rounded-full font-medium`}
               >
                 Ver Catálogo
               </Link>
